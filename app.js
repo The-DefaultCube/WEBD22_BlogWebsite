60b153d6f45a323b305a82ae10bdfdc209d6d996
MANISH <71868648+The-DefaultCube@users.noreply.github.com>
2022-06-25 01:06:11 +0530

    added database

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const path = require("path");
 const _ = require("lodash");
 /*----------------------------------------------------------------------------*/
@@ -21,25 +22,34 @@ app.use(
 );
 /*----------------------------------------------------------------------------*/
 app.listen(3000, function () {
-  console.log("Server started on port 3000");
 });
 /*----------------------------------------------------------------------------*/
-/*-----------------------------------------------------------------------------*/
+/*-----------------------------DATABASE-------------------------------------*/
+mongoose.connect("mongodb://localhost:27017/blogWebsiteDB");
+
+const postSchema = new mongoose.Schema({
+  postTitle: String,
+  postContent: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 /*----------------------------------------------------------------------------*/
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.pat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.pat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdietellentesque adipiscing.pat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci or neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu n risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let postArray = [];
 /*---------------------------------GET--------------------------------------*/
 /*---------------------------HOME----------------------------------------*/
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    postArray: postArray,
+  Post.find({}, (err, foundPosts) => {
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      postArray: foundPosts,
+    });
   });
 });
 /*----------------------------------------------------------------------------*/
@@ -61,29 +71,44 @@ app.get("/contact", (req, res) => {
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
-
-app.get("/post/:postName", (req, res) => {
-  console.log(req.params.postName);
-  const postName = _.lowerCase(req.params.postName);
-  postArray.forEach((post) => {
-    const presentPostName = _.lowerCase(post.postTitle);
-    if (presentPostName === postName) {
+/*--------------------------View Post--------------------------------------------*/
+app.get("/post/:postId", (req, res) => {
+  const postId = req.params.postId;
+  //find that post by its id
+  Post.findOne({ _id: postId }, (err, foundPost) => {
+    if (!err && foundPost) {
       res.render("post", {
-        postTitle: presentPostName,
-        postContent: post.postContent,
+        postTitle: foundPost.postTitle,
+        postContent: foundPost.postContent,
+        postId: foundPost._id
       });
     } else {
-      console.log("post named " + postName + " does not exist.");
+      res.send("NO post found!");
     }
   });
 });
 /*---------------------------------POST--------------------------------------*/
-/*--------------------------CONTACT--------------------------------------------*/
+/*--------------------------COMPOSE NEW POST--------------------------------------------*/
 app.post("/compose", (req, res) => {
-  const newPost = {
+  const newPost = new Post({
     postTitle: req.body.postTitle,
     postContent: req.body.postContent,
-  };
-  postArray.push(newPost);
-  res.redirect("/");
+  });
+  newPost.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
+
+/*---------------------------------POST--------------------------------------*/
+/*--------------------------DELETE THE POST-----------------------------------*/
+app.post("/delete", (req, res) => {
+  const postIdToDelete = req.body.delete;
+  Post.findByIdAndDelete(postIdToDelete, (err, deletedPost)=>{
+    if(!err){
+      res.redirect("/");
+    }
+  })
+});
+
